1af8f085077ec42dd05a8ae0aa3423c8bff26d7c
RakhaMaulana26 <rakhamkp@gmail.com>
2025-11-24 16:57:04 +0700

    cross-plattform

--- src/components/CursorTrail.jsx
+++ src/components/CursorTrail.jsx
@@ -4,16 +4,10 @@ import './CursorTrail.css'
 
 export default function CursorTrail() {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
-  const [isPointer, setIsPointer] = useState(false)
 
   useEffect(() => {
     const handleMouseMove = (e) => {
       setMousePosition({ x: e.clientX, y: e.clientY })
-      
-      // Check if hovering over clickable element
-      const target = e.target
-      const isClickable = target.closest('a, button, .clickable')
-      setIsPointer(!!isClickable)
     }
 
     window.addEventListener('mousemove', handleMouseMove)
@@ -22,13 +16,12 @@ export default function CursorTrail() {
 
   return (
     <>
-      {/* Main cursor */}
+      {/* Custom cursor image */}
       <motion.div
         className="custom-cursor"
         animate={{
-          x: mousePosition.x - 10,
-          y: mousePosition.y - 10,
-          scale: isPointer ? 1.5 : 1,
+          x: mousePosition.x - 16,
+          y: mousePosition.y - 16,
         }}
         transition={{
           type: "spring",
@@ -36,21 +29,13 @@ export default function CursorTrail() {
           damping: 28,
           mass: 0.5
         }}
-      />
-      
-      {/* Trailing cursor */}
-      <motion.div
-        className="custom-cursor-trail"
-        animate={{
-          x: mousePosition.x - 20,
-          y: mousePosition.y - 20,
-          scale: isPointer ? 2 : 1,
-        }}
-        transition={{
-          type: "spring",
-          stiffness: 150,
-          damping: 15,
-          mass: 0.8
+        style={{
+          backgroundImage: 'url(/cursor.png)',
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          width: '32px',
+          height: '32px',
         }}
       />
     </>
